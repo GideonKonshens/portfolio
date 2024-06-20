888c79c48ad7f732294cc04f0c587d661588673c
HighSchoolTatum <gideonkonshens0@gmail.com>
2024-06-20 21:11:10 +0300

    Added Firebase
    Installed Firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,28 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfRu56pSw3838qXpDBfwq6J34Wwgt_umk",
+  authDomain: "folio-98f5a.firebaseapp.com",
+  projectId: "folio-98f5a",
+  storageBucket: "folio-98f5a.appspot.com",
+  messagingSenderId: "125193028819",
+  appId: "1:125193028819:web:18067375c68cb92fef8026",
+  measurementId: "G-HW6C76W9TK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
